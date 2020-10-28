d2cc5dc833c1e202da977b98f3d453f06666d94f
HoaiBao <rorolo2011@gmail.com>
2020-10-28 22:24:54 +0700

    Updates

---
+++ precache-manifest.2996523543fab7a823a2e8fafb230d87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13794a06e08e3d905212cabd4a535fa7",
+    "url": "/Sprint-Retrospective/index.html"
+  },
+  {
+    "revision": "f54c22b90851aaf4c7a9",
+    "url": "/Sprint-Retrospective/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "8a07fd542ced27af0eac",
+    "url": "/Sprint-Retrospective/static/css/main.fd19d239.chunk.css"
+  },
+  {
+    "revision": "f54c22b90851aaf4c7a9",
+    "url": "/Sprint-Retrospective/static/js/2.020269ea.chunk.js"
+  },
+  {
+    "revision": "ce53c4b16522e4a5817476f862996bac",
+    "url": "/Sprint-Retrospective/static/js/2.020269ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a07fd542ced27af0eac",
+    "url": "/Sprint-Retrospective/static/js/main.9c670a11.chunk.js"
+  },
+  {
+    "revision": "3b9e94bd5ec1486ad09c",
+    "url": "/Sprint-Retrospective/static/js/runtime-main.86cb14fc.js"
+  },
+  {
+    "revision": "75f0ef31b366c601a78a71bd373e043f",
+    "url": "/Sprint-Retrospective/static/media/sort.75f0ef31.svg"
+  }
+]);
